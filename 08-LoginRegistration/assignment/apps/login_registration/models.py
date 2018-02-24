ba302764ab6b28c750a6e255eb59a6ffbe1385c1
Chia T <tham_oh@hotmail.com>
2018-02-23 22:38:49 -0600

    add logic and error messge

--- 08-LoginRegistration/assignment/apps/login_registration/models.py
+++ 08-LoginRegistration/assignment/apps/login_registration/models.py
@@ -4,7 +4,39 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
+class UserManager(models.Manager):
+    def registration_validator(self, postData):
+        errors = {}
+        if len(postData['firstname']) < 2:
+            errors["firstname"] = "First name should be more than 2 characters"
+        if len(postData['lastname']) < 2:
+            errors["lastname"] = "Last name should be more than 2 characters"
+        elif postData['lastname'].isalpha() == False:
+            errors['lastname'] = "Last name should be all alpha characters"
+        if len(postData['emailaddress']) < 7:
+            errors["email"] = "Email address should be more than 7 characters"
+        elif not EMAIL_REGEX.match(postData['emailaddress']):
+            errors["email"] = "Email should be a valid email address"
+        if len(postData['password1']) < 8:
+            errors["password"] = "Password should be more than 8 characters"
+        elif len(postData['password2']) < 8:
+            errors["password"] = "Password should be more than 8 characters"
+        if postData['password1'] != postData['password2']:
+            errors["passwordNotMatch"] = "Password and confirm password should be matched"
+        return errors
+    
+    def login_validator(self, postData):
+        errors = {}
+        if len(postData['email']) < 8:
+            errors["email"] = "Login Email address should be more than 8 characters"
+        elif not EMAIL_REGEX.match(postData['email']):
+            errors["email"] = "Login Email should be a valid email address"
+        if len(postData['password']) < 8:
+            errors["password"] = "Login Password should be more than 8 characters"
+        return errors
 
 
 class User(models.Model):
@@ -15,3 +47,5 @@ class User(models.Model):
     password = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
+
+    objects = UserManager()
--- 08-LoginRegistration/assignment/apps/login_registration/views.py
+++ 08-LoginRegistration/assignment/apps/login_registration/views.py
@@ -11,20 +11,55 @@ from .models import User
  
 def index(request):
     print "* index"
+    errors = {}
+    if len(errors):
+        for tag, error in errors.iteritems():
+            messages.error(request, error, extra_tags=tag)
     return render(request, "login_registration/index.html")
 
 def registration(request):
     print "* registration"
     if request.method == "POST":
-        User.objects.create(first_name=request.POST.get('firstname'), last_name=request.POST.get('lastname'), email_address=request.POST.get('emailaddress'), password=request.POST.get('password1'))
-        return redirect('/login')
+        errors = User.objects.registration_validator(request.POST)
+        if len(errors):
+            for tag, error in errors.iteritems():
+                messages.error(request, error, extra_tags=tag)
+            return redirect('/')
+        else:
+            User.objects.create(first_name=request.POST.get('firstname'), last_name=request.POST.get('lastname'), email_address=request.POST.get('emailaddress'), password=request.POST.get('password1'))
+            context = {
+                "all_users" : User.objects.all(),
+                "name"      : request.POST.get('firstname')
+            }
+            return render(request, "login_registration/dashboard.html", context)
     return redirect('/')
 
 def login(request):
     print "* login"
-    context = {
-        "all_users" : User.objects.all(),
-        "name"      : request.POST.get('firstname')
-    }
-    return render(request, "login_registration/dashboard.html", context)
+    temp_t = request.POST.get('email')
+    temp_p = request.POST.get('password')
+    if request.method == "POST":
+        errors = User.objects.login_validator(request.POST)
+        if len(errors):
+            for tag, error in errors.iteritems():
+                messages.error(request, error, extra_tags=tag)
+            return redirect('/')
+        else:
+            c1 = User.objects.filter(email_address=temp_t)
+            if len(c1) > 0:
+                c2 = User.objects.get(email_address=temp_t)
+                if c2.password != temp_p:
+                    errors["NotFound"] = "Email and Password should be match"
+            else:
+                errors["NotFound"] = "Email should be registed in our system"
+            if len(errors):
+                for tag, error in errors.iteritems():
+                    messages.error(request, error, extra_tags=tag)
+                return redirect('/')
+            else:
+                context = {
+                    "all_users" : User.objects.all(),
+                    "name"      : c2.first_name
+                }
+                return render(request, "login_registration/dashboard.html", context)
     return redirect('/')
